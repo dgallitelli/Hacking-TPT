ea281a9bfbe2da863b987f2ba92f4c8e28ef1c29
Davide Gallitelli <davidegallitelli@gmail.com>
2018-01-31 22:33:00 +0100

    Updated run script and removed src old folder

--- Code/run.sh
+++ Code/run.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+echo "### BUILDING LOADER .... ###"
+cd ./src/loader/
+./build.sh
+echo "### LOADER IS BUILT. ###"
+
+echo "### BUILDING MIRAI WITH OPTIONS $1 $2"
 cd ./src/mirai/
-./build.sh debug telnet
+./build.sh $1 $2
 cd ../..
+echo "### MIRAI IS BUILT. ###"
+
+echo "### LAUNCHING THE CONTAINERS ###"
 docker-compose up --build
